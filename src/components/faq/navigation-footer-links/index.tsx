e0be4b0421007424819bbedfe599f091543de2b5
Behçet Muhammed <emiraydemirsyr@gmail.com>
2025-08-05 17:37:19 +0300

    Refactor [Tüm alt sayfalar] | Responsive düzenlemeler yapıldı v2

--- src/components/faq/navigation-footer-links/index.tsx
+++ src/components/faq/navigation-footer-links/index.tsx
@@ -1,14 +1,45 @@
 import React from "react";
 import styles from "./../style.module.css";
 import { IkasNavigationLink, Link } from "@ikas/storefront";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/free-mode";
+import { useScreen } from "src/utils/hooks/useScreen";
 
 const NavigationFooterLinks = ({
   footer_links,
 }: {
   footer_links: IkasNavigationLink[] | undefined;
 }) => {
+  const { isMobile } = useScreen();
+
   if (!footer_links) {
     return null;
+  }
+
+  if (isMobile) {
+    return (
+      <div className={styles.footer_link_swiper}>
+        <Swiper
+          spaceBetween={10}
+          slidesPerView="auto"
+          freeMode={true}
+          modules={[FreeMode]}
+          className="mySwiper"
+        >
+          {footer_links?.map((item, index) => {
+            return (
+              <SwiperSlide key={index} className={styles.footer_link_slide}>
+                <Link href={item.href}>
+                  <a>{item.label}</a>
+                </Link>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      </div>
+    );
   }
 
   return (
